197699d8b063a68e83eeff1933cdd2549bc6198a
ovovvvvv <ovovvvvv@naver.com>
2023-10-21 15:12:26 +0900

    newsapi호출

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=432d8f033cc64de8acbf5fb760576266',
       );
       setData(response.data);
     } catch (e) {
